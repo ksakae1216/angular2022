adb747cc099c3f9ec9479cc0ebca594dc7d8368d
kotaro_sakae <ksakae1216@gmail.com>
2023-02-07 20:22:26 +0900

    feat(feature-top): add feature-top library

---
+++ libs/feature-top/README.md
@@ -0,0 +1,7 @@
+# feature-top
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test feature-top` to execute the unit tests.
---
+++ libs/feature-top/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FeatureTopComponent } from './feature-top/feature-top.component';
+
+export const featureTopRoutes: Route[] = [
+  { path: '', component: FeatureTopComponent },
+];

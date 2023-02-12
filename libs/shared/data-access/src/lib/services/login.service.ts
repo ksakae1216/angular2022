4182a82d3c34a88d332045962322ddc6da345dd4
kotaro_sakae <ksakae1216@gmail.com>
2023-02-12 09:02:30 +0900

    feat(shared/data-access): implements login service

--- libs/shared/data-access/src/lib/services/login.service.ts
+++ libs/shared/data-access/src/lib/services/login.service.ts
@@ -1,6 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Login } from '../models/login.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoginService {}
+export class LoginService {
+  constructor(private http: HttpClient) {}
+
+  postLogin(loginId: string, password: string) {
+    return this.http.post<Login>('/api/login', { loginId, password });
+  }
+}
--- libs/shared/data-access/src/lib/shared-data-access.module.ts
+++ libs/shared/data-access/src/lib/shared-data-access.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { LoginService } from './services/login.service';
 
 @NgModule({
   imports: [CommonModule],
+  providers: [LoginService],
 })
 export class SharedDataAccessModule {}

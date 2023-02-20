bf27db7144b1d9094054e655a563919b410c1bc7
Sakae Kotaro <ksakae1216@gmail.com>
2023-02-20 21:35:38 +0900

    test(feature-login): implements login-container and store (#53)

--- libs/feature-login/src/lib/containers/login-container/login-container.component.spec.ts
+++ libs/feature-login/src/lib/containers/login-container/login-container.component.spec.ts
@@ -1,14 +1,33 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 import { LoginContainerComponent } from './login-container.component';
+import { LoginContainerStore } from './login-container.store';
 
 describe('LoginContainerComponent', () => {
   let component: LoginContainerComponent;
+  let componentStore: LoginContainerStore;
   let fixture: ComponentFixture<LoginContainerComponent>;
 
-  beforeEach(async() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [ LoginContainerComponent ]
+      declarations: [LoginContainerComponent, LoginFormComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).overrideComponent(LoginContainerComponent, {
+      set: {
+        providers: [
+          {
+            provide: LoginContainerStore,
+            useValue: {
+              store$: of({}),
+              login: jest.fn(),
+            },
+          },
+        ],
+      },
     });
 
     await TestBed.compileComponents();
@@ -17,11 +36,44 @@ describe('LoginContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginContainerComponent);
     component = fixture.componentInstance;
+    componentStore = fixture.debugElement.injector.get(LoginContainerStore);
 
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(componentStore).toBeTruthy();
+  });
+
+  it('should call LoginContainerComponent.login()', () => {
+    jest.spyOn(component, 'login');
+
+    (
+      fixture.debugElement.query(By.directive(LoginFormComponent))
+        .componentInstance as LoginFormComponent
+    ).login.emit({
+      loginId: 'test1',
+      password: 'Password1',
+    });
+
+    expect(component.login).toHaveBeenCalledWith({
+      loginId: 'test1',
+      password: 'Password1',
+    });
+  });
+
+  it('should call LoginContainerStore.login()', () => {
+    jest.spyOn(componentStore, 'login');
+
+    component.login({
+      loginId: 'test1',
+      password: 'Password1',
+    });
+
+    expect(componentStore.login).toHaveBeenCalledWith({
+      loginId: 'test1',
+      password: 'Password1',
+    });
   });
 });

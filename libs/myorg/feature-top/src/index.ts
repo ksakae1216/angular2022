bcebce8b3c1d5306af50daa7f09b64d727ca5532
kotaro_sakae <ksakae1216@gmail.com>
2023-03-22 21:26:04 +0900

    feat(myorg-feature-top): rename top component

--- libs/myorg/feature-top/src/index.ts
+++ libs/myorg/feature-top/src/index.ts
@@ -1,2 +1,3 @@
-export * from './lib/components/feature-top/feature-top.component';
+export * from './lib/components/top/top.component';
 export * from './lib/lib.routes';
+export * from './lib/pages/top-page/top-page.component';

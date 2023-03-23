2236beb20483ffcf496e5bcb6fcc71a207f0bca2
kotaro_sakae <ksakae1216@gmail.com>
2023-03-23 21:41:06 +0900

    feat(myorg-feature-top): add header-page

---
+++ libs/myorg/feature-top/src/lib/pages/header-page/header-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'myorg-header-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header-page.component.html',
+})
+export class HeaderPageComponent {}

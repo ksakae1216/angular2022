c66566322e7aacde84e0a5c563fe8ee51d77b103
kotaro_sakae <ksakae1216@gmail.com>
2023-03-23 21:41:54 +0900

    feat(myorg-feature-top): implement main-layout

--- libs/myorg/feature-top/src/lib/layouts/main-layout/main-layout.component.ts
+++ libs/myorg/feature-top/src/lib/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MyorgTopComponent } from '../../components/top/top.component';
+import { HeaderPageComponent } from '../../pages/header-page/header-page.component';
 
 @Component({
   selector: 'myorg-main-layout',
   standalone: true,
-  imports: [MyorgTopComponent],
+  imports: [HeaderPageComponent, MyorgTopComponent],
   templateUrl: './main-layout.component.html',
   styleUrls: [],
 })

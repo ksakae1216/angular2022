bc382ed9d3f3ce55992b05eb61cd31e35a26d5d5
kotaro_sakae <ksakae1216@gmail.com>
2023-03-25 13:17:28 +0900

    feat(myorg-feature-top): generate body-page

--- libs/myorg/feature-top/src/lib/layouts/main-layout/main-layout.component.ts
+++ libs/myorg/feature-top/src/lib/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MyorgTopComponent } from '../../components/top/top.component';
+import { BodyPageComponent } from '../../pages/body-page/body-page.component';
 import { HeaderPageComponent } from '../../pages/header-page/header-page.component';
 
 @Component({
   selector: 'myorg-main-layout',
   standalone: true,
-  imports: [HeaderPageComponent, MyorgTopComponent],
+  imports: [HeaderPageComponent, BodyPageComponent, MyorgTopComponent],
   templateUrl: './main-layout.component.html',
   styleUrls: [],
 })
---
+++ libs/myorg/feature-top/src/lib/pages/body-page/body-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'myorg-body-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './body-page.component.html',
+  styles: [],
+})
+export class BodyPageComponent {}

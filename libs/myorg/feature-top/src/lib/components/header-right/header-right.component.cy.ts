19df53d7e4968552f54664bb3874116676a02aef
Sakae Kotaro <ksakae1216@gmail.com>
2023-05-03 13:37:43 +0900

    feat(myorg-feature-top): implement header right component (#89)
    * feat(myorg): add account_circle.svg
    
    * feat(myorg-feature-top): implement account_circle.svg
    
    * feat(myorg-feature-top): fix cypress component test error
    
    * feat(myorg-feature-top): implement header right component

--- libs/myorg/feature-top/src/lib/components/header-right/header-right.component.cy.ts
+++ libs/myorg/feature-top/src/lib/components/header-right/header-right.component.cy.ts
@@ -3,12 +3,30 @@ import { HeaderRightComponent } from './header-right.component';
 describe('HeaderRightComponent', () => {
   it('should render component', () => {
     setup();
+
+    cy.get('img').should('exist');
+  });
+
+  it('should open menu', () => {
+    setup();
+
+    cy.get('img').click();
+
+    cy.get('button').should('exist');
+  });
+
+  it('render menu items', () => {
+    setup();
+
+    cy.get('img').click();
+
+    cy.get('[name="settings"]').should('exist');
+    cy.get('[name="dummy"]').should('exist');
   });
 });
 
-function setup(props?: Partial<HeaderRightComponent>) {
+function setup() {
   cy.mount(HeaderRightComponent, {
-    imports: [],
     componentProperties: {},
   });
 }

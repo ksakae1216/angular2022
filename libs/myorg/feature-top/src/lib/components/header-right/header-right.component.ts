48584558c8ba751af48acfedb47b4969250a0073
kotaro_sakae <ksakae1216@gmail.com>
2023-05-03 13:35:56 +0900

    feat(myorg-feature-top): implement header right component

--- libs/myorg/feature-top/src/lib/components/header-right/header-right.component.ts
+++ libs/myorg/feature-top/src/lib/components/header-right/header-right.component.ts
@@ -1,10 +1,12 @@
+import { CdkMenuModule } from '@angular/cdk/menu';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'myorg-header-right',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CdkMenuModule, MatIconModule],
   templateUrl: './header-right.component.html',
 })
 export class HeaderRightComponent {}

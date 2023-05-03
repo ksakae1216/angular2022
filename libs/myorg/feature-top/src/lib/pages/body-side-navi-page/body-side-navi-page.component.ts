e5905d771f501990be6e615c66812630a54b23d5
kotaro_sakae <ksakae1216@gmail.com>
2023-05-03 14:12:41 +0900

    feat(myorg-feature-top): generate body-side-navi-page ``` nx generate @myorg/myorg-plugin:component body-side-navi-page --project=myorg-feature-top --path=libs/myorg/feature-top/src/lib/pages --no-interactive ```

---
+++ libs/myorg/feature-top/src/lib/pages/body-side-navi-page/body-side-navi-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'myorg-body-side-navi-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './body-side-navi-page.component.html',
+})
+export class BodySideNaviPageComponent {}

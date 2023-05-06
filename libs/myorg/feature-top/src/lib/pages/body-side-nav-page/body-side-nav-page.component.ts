5a77b3a2bce7b4d455934c269443a8d8ec1e8758
kotaro_sakae <ksakae1216@gmail.com>
2023-05-07 08:34:24 +0900

    refactor(myorg-feature-top): rename side-nav

---
+++ libs/myorg/feature-top/src/lib/pages/body-side-nav-page/body-side-nav-page.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myorg-body-side-nav-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './body-side-nav-page.component.html',
+})
+export class BodySideNavPageComponent {}

5088b08de43442cfee886687c7248a5c4ec82902
kotaro_sakae <ksakae1216@gmail.com>
2023-07-29 14:06:36 +0900

    feat(myorg-feature-portal): generate top-layout ``` nx generate @myorg/myorg-plugin:component top-layout --project=myorg-feature-portal --path=libs/myorg/feature-portal/src/lib/layouts --no-interactive ```

---
+++ libs/myorg/feature-portal/src/lib/layouts/top-layout/top-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'myorg-top-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './top-layout.component.html',
+})
+export class TopLayoutComponent {}

d1a4908219d5a184b0ab286c12827dc430e30116
kotaro_sakae <ksakae1216@gmail.com>
2023-09-03 10:15:50 +0900

    feat(myorg-feature-top): change router-outlet

--- libs/myorg/feature-top/src/index.ts
+++ libs/myorg/feature-top/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/components';
 export * from './lib/layouts/main-layout/main-layout.component';
 export * from './lib/lib.routes';
+export * from './lib/pages/body-main-page/body-main-page.component';

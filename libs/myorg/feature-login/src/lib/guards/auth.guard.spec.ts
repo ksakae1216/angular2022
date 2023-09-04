448d58ea18ba76a7dfcf47903bbcc059f3799a8b
Sakae Kotaro <ksakae1216@gmail.com>
2023-09-04 20:56:00 +0900

    test(myorg-feature-login): implement test of auth.guard (#104)

--- libs/myorg/feature-login/src/lib/guards/auth.guard.spec.ts
+++ libs/myorg/feature-login/src/lib/guards/auth.guard.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { Router } from '@angular/router';
 import { AuthService } from '@myorg/myorg/shared/data-access';
+import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
-  // const executeGuard: CanActivateFn = (...guardParameters) =>
-  //   TestBed.runInInjectionContext(() => AuthGuard(...guardParameters));
-
   let authService: AuthService;
   let router: Router;
 
@@ -22,9 +20,24 @@ describe('AuthGuard', () => {
   it('should return true if token exist', () => {
     jest.spyOn(authService, 'isTokenExist').mockReturnValueOnce(true);
 
-    // angular version 15.1以上にしたらコメント外す
-    // TestBed.runInInjectionContext(() => AuthGuard()).subscribe((result) => {
-    //   expect(result).toBe(true);
-    // });
+    TestBed.runInInjectionContext(() => AuthGuard()).subscribe((result) => {
+      expect(result).toBe(true);
+    });
+  });
+
+  it('should return false if token does not exist', () => {
+    jest.spyOn(authService, 'isTokenExist').mockReturnValueOnce(false);
+
+    TestBed.runInInjectionContext(() => AuthGuard()).subscribe((result) => {
+      expect(result).toBe(false);
+    });
+  });
+
+  it('should navigate to login page if token does not exist', () => {
+    jest.spyOn(authService, 'isTokenExist').mockReturnValueOnce(false);
+
+    TestBed.runInInjectionContext(() => AuthGuard()).subscribe(() => {
+      expect(router.navigateByUrl).toHaveBeenCalledWith('/login');
+    });
   });
 });

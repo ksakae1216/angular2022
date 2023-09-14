e5dd864391f88945925b3169e916607142af658c
kotaro_sakae <ksakae1216@gmail.com>
2023-09-14 16:57:51 +0900

    feat(myorg): implement list api response

--- apps/myorg/src/mocks/hundlers.ts
+++ apps/myorg/src/mocks/hundlers.ts
@@ -1,5 +1,4 @@
+import { listApiMocks } from './list/list-api';
 import { loginApiMocks } from './login/login-api';
 
-export const hundlers = [
-  ...loginApiMocks,
-];
+export const hundlers = [...loginApiMocks, ...listApiMocks];

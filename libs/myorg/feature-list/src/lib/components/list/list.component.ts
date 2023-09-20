b4071c9b0d3ae48b143e0529273c9d8c5959e3f9
kotaro_sakae <ksakae1216@gmail.com>
2023-09-20 20:27:13 +0900

    feat(myorg-feature-list): implement list table to list.component

--- libs/myorg/feature-list/src/lib/components/list/list.component.ts
+++ libs/myorg/feature-list/src/lib/components/list/list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ElementList } from '@myorg/myorg/shared/api';
 
 @Component({
   selector: 'myorg-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CdkTableModule],
   templateUrl: './list.component.html',
 })
-export class ListComponent {}
+export class ListComponent {
+  @Input() elementList: ElementList[] = [];
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+}

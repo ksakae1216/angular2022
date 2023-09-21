96d70a72ee08f776bd21d2aee27ac0b5ec62e7c0
Sakae Kotaro <ksakae1216@gmail.com>
2023-09-21 20:59:37 +0900

    test(myorg-feature-list): implement test to list.component (#111)

--- libs/myorg/feature-list/src/lib/components/list/list.component.cy.ts
+++ libs/myorg/feature-list/src/lib/components/list/list.component.cy.ts
@@ -4,11 +4,24 @@ describe('ListComponent', () => {
   it('should render component', () => {
     setup();
   });
+
+  it('should render list of 3', () => {
+    setup({
+      elementList: [
+        { position: '1', name: 'Hydrogen', weight: '1.0079', symbol: 'H' },
+        { position: '2', name: 'Helium', weight: '4.0026', symbol: 'He' },
+        { position: '3', name: 'Lithium', weight: '6.941', symbol: 'Li' },
+      ],
+    });
+    cy.get('tbody>tr').should('have.length', 3);
+  });
 });
 
 function setup(props?: Partial<ListComponent>) {
   cy.mount(ListComponent, {
     imports: [],
-    componentProperties: {},
+    componentProperties: {
+      elementList: props?.elementList ?? [],
+    },
   });
 }

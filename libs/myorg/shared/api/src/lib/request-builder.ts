2e045f3460beff7aa67ec17cf57d65944b83fafe
kotaro_sakae <ksakae1216@gmail.com>
2025-11-02 11:31:14 +0900

    chore: run Nx and Angular migration commands to Angular 18
    - Run nx migrate @nx/angular@18
    - Run nx migrate @angular/core@18 @angular/cli@18
    - Run nx migrate @angular/material@18
    - Run nx migrate nx@18
    - Run nx migrate --run-migrations
    
    This updates package.json dependencies and applies automatic code migrations
    including HTTP provider migration for Angular 18 compatibility.
    
    Also includes manual updates:
    - Update remaining dependencies to Angular 18 compatible versions
    - Update library peerDependencies to Angular 18
    - Fix tapResponse import from @ngrx/operators
    - Update test-setup.ts for jest-preset-angular 15 compatibility

--- libs/myorg/shared/api/src/lib/request-builder.ts
+++ libs/myorg/shared/api/src/lib/request-builder.ts
@@ -1,12 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-import {
-  HttpRequest,
-  HttpParameterCodec,
-  HttpParams,
-  HttpHeaders,
-  HttpContext,
-} from '@angular/common/http';
+import { HttpRequest, HttpParameterCodec, HttpParams, HttpHeaders, HttpContext } from '@angular/common/http';
 
 /**
  * Custom parameter codec to correctly handle the plus sign in parameter
